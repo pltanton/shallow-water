37ae0d49f5a16bb21a4e10ab6b38b4c42ed2e0cd
Plotnikov Anton <plotnikovanton@gmail.com>
2016-12-05 21:59:28 +0300

    Removed usless main execution

--- core/runge_kutta.py
+++ core/runge_kutta.py
@@ -30,37 +30,3 @@ class RungeKutta(evaluator.Evaluator):
             time += dt
 
             yield h, u, v, time
-
-
-if __name__ == '__main__':
-    import sys
-
-    ## Create a GL View widget to display data
-    app = QtGui.QApplication([])
-    w = gl.GLViewWidget()
-    w.show()
-    w.setWindowTitle('Shallow Water euler')
-    w.setCameraPosition(distance=25)
-
-    # TODO: need to refactor, just for test here
-    h = base.H_0
-    u = base.U_0
-    v = base.V_0
-    g = base.G
-    dt = DT
-    endTime = 0.3
-    x, y = np.mgrid[:base.N, :base.N]
-    droplet_x, droplet_y = 50, 50
-    rr = (x - droplet_x) ** 2 + (y - droplet_y) ** 2
-    h[rr < 10 ** 2] = 3
-    # Init generator
-    rungekutta = RungeKutta.evolve(h, u, v, g, dt)
-    h, u, v, time = next(rungekutta)
-
-    p = gl.GLSurfacePlotItem(z=h, shader='heightColor', color=(0.5, 0.5, 1, 1),
-                             smooth=False)
-    p.shader()['colorMap'] = np.array([0.2, 2, 0.5, 0.2, 1, 1, 0.2, 0, 2])
-    # p.scale(2,2,1)
-    p.translate(-base.N / 8, -base.N / 8, 0)
-    p.scale(0.25, 0.25, 1.0)
-    w.addItem(p)
